b762a6fc59b12e1e35c97abe0419737bf624ae9c
lordran <lordran@lordran.dev>
2024-11-30 19:46:24 -0600

    Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,4 +18,16 @@
 	{@render children()}
 </main>
 
-<footer></footer>
+<footer>
+	<ul>
+		<li>
+			<a href="https://github.com/DanteDeLordran" target="_blank">GitHub</a>
+		</li>
+		<li>
+			<a href="https://www.linkedin.com/in/dantelopezlugo/" target="_blank">LinkedIn</a>
+		</li>
+		<li>
+			<a href="https://twitter.com/DanteDeLordran" target="_blank">Twitter</a>
+		</li>
+	</ul>
+</footer>
