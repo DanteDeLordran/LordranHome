5cc9ec3ff6f0111e53e51241a74283b5690323b8
lordran <lordran@lordran.dev>
2024-12-26 11:53:48 -0600

    Separated footer and header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,14 @@
 <script lang="ts">
-	import "../app.css";
-	import { routes } from "../utils/routes";
-	let { children } = $props();
+import "../app.css";
+import Footer from "../components/Footer.svelte";
+import Header from "../components/Header.svelte";
+const { children } = $props();
 </script>
 
-<header>
-	<ul>
-		{#each routes as route}
-			<li>
-				<a href={route.path}>{route.name}</a>
-			</li>
-		{/each}
-	</ul>
-</header>
+<Header />
 
-<main>
+<main class="flex flex-col items-center justify-center mt-32">
 	{@render children()}
 </main>
 
-<footer>
-	<ul>
-		<li>
-			<a href="https://github.com/DanteDeLordran" target="_blank">GitHub</a>
-		</li>
-		<li>
-			<a href="https://www.linkedin.com/in/dantelopezlugo/" target="_blank">LinkedIn</a>
-		</li>
-		<li>
-			<a href="https://twitter.com/DanteDeLordran" target="_blank">Twitter</a>
-		</li>
-	</ul>
-</footer>
+<Footer />
