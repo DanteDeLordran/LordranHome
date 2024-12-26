bdd908a05d8e277744fd98c26fd151898822afb3
lordran <lordran@lordran.dev>
2024-12-26 10:23:10 -0600

    Footer and Header

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer>
+	<ul>
+		<li>
+			<a href="https://github.com/DanteDeLordran" target="_blank"
+				>GitHub</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://www.linkedin.com/in/dantelopezlugo/"
+				target="_blank">LinkedIn</a
+			>
+		</li>
+		<li>
+			<a href="https://twitter.com/DanteDeLordran" target="_blank"
+				>Twitter</a
+			>
+		</li>
+	</ul>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+import { routes } from "../utils/routes";
+</script>
+
+<header class="container">
+    <nav
+        class="flex justify-between md:justify-around py-4 bg-white/80 backdrop-blur-md shadow-md w-full px-10 fixed top-0 left-0 right-0 z-10 md:px-3"
+    >
+        <div class="flex items-center">
+            <a class="cursor-pointer" href="/">
+                <h3 class="text-2xl font-medium text-blue-500">
+                    DanteDeLordran
+                </h3>
+            </a>
+        </div>
+        <div
+            class="ml-auto items-center md:space-x-8 justify-center justify-items-start md:justify-items-center md:flex md:pt-2 w-full md:w-auto left-0 top-16 px-5 md:px-10 py-3 md:py-0 border-t md:border-t-0"
+        >
+            {#each routes as route}
+                <a
+                    class="flex text-gray-600 hover:text-blue-500 cursor-pointer transition-colors duration-300"
+                    href={route.path}>{route.name}</a
+                >
+            {/each}
+        </div>
+    </nav>
+</header>
