2088b4ac7c754ee7bb8aa0c3bbdc07d53a762540
lordran <lordran@lordran.dev>
2024-12-26 13:16:17 -0600

    Added correct colors

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,34 +3,34 @@
     import { routes } from "../utils/routes";
 </script>
 
-<footer class="bg-gray-800 pt-5 sm:mt-10 w-full">
+<footer class="bg-[#0A1026] pt-5 sm:mt-10 w-full">
     <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
         <!-- Col-1 -->
-        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+        <div class="p-4 w-1/2 sm:w-4/12 md:w-3/12">
             <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+            <div class="text-xs uppercase text-white hover:text-[#01C979] mb-5 cursor-pointer transition-colors duration-300">
                 Sections
             </div>
 
             {#each routes as route}
                 <a
                     href={route.path}
-                    class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+                    class="my-2 block text-white hover:text-[#01C979] text-sm cursor-pointer transition-colors duration-300"
                 >
                     {route.name}
                 </a>
             {/each}
         </div>
 
-        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+        <div class="p-4 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="text-xs uppercase text-white hover:text-[#01C979] font-medium mb-5 cursor-pointer transition-colors duration-300">
                 Contact
             </div>
             {#each contacts as contact}
                 <a
                     href={contact.path}
                     target="_blank"
-                    class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+                    class="my-2 block text-white hover:text-[#01C979] text-sm cursor-pointer transition-colors duration-300"
                 >
                     {contact.name}
                 </a>
@@ -39,9 +39,9 @@
     </div>
 
     <!-- Copyright Bar -->
-    <div class="pt-2">
+    <div class="pt-1">
         <div
-            class="flex pb-5 px-3 m-auto pt-5 border-t border-gray-500 text-gray-400 text-sm flex-col md:flex-row max-w-6xl"
+            class="flex pb-5 px-3 m-auto pt-4 border-t border-[#01C979] text-white text-sm flex-col md:flex-row max-w-6xl"
         >
             <div class="mt-2">Last updated 12-26-2024</div>
         </div>
