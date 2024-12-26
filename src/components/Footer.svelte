b8c352491d2c312c75680aeca940646837ef1920
lordran <lordran@lordran.dev>
2024-12-26 11:49:40 -0600

    Added a coontacts file

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,20 +1,49 @@
-<footer>
-	<ul>
-		<li>
-			<a href="https://github.com/DanteDeLordran" target="_blank"
-				>GitHub</a
-			>
-		</li>
-		<li>
-			<a
-				href="https://www.linkedin.com/in/dantelopezlugo/"
-				target="_blank">LinkedIn</a
-			>
-		</li>
-		<li>
-			<a href="https://twitter.com/DanteDeLordran" target="_blank"
-				>Twitter</a
-			>
-		</li>
-	</ul>
+<script>
+    import { contacts } from "../utils/contacts";
+    import { routes } from "../utils/routes";
+</script>
+
+<footer class="bg-gray-800 pt-5 sm:mt-10 w-full">
+    <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
+        <!-- Col-1 -->
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <!-- Col Title -->
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                Sections
+            </div>
+
+            {#each routes as route}
+                <a
+                    href={route.path}
+                    class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+                >
+                    {route.name}
+                </a>
+            {/each}
+        </div>
+
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                Contact
+            </div>
+            {#each contacts as contact}
+                <a
+                    href={contact.path}
+                    target="_blank"
+                    class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700"
+                >
+                    {contact.name}
+                </a>
+            {/each}
+        </div>
+    </div>
+
+    <!-- Copyright Bar -->
+    <div class="pt-2">
+        <div
+            class="flex pb-5 px-3 m-auto pt-5 border-t border-gray-500 text-gray-400 text-sm flex-col md:flex-row max-w-6xl"
+        >
+            <div class="mt-2">Last updated 12-26-2024</div>
+        </div>
+    </div>
 </footer>
---
+++ src/utils/contacts.ts
@@ -0,0 +1,18 @@
+
+
+export const contacts: { path: string, name: string }[] = [
+
+    {
+        name: 'GitHub',
+        path: 'https://github.com/DanteDeLordran'
+    },
+    {
+        name: 'Twitter',
+        path: 'https://twitter.com/DanteDeLordran'
+    },
+    {
+        name: 'LinkedIn',
+        path: 'https://www.linkedin.com/in/dantelopezlugo/'
+    },
+    
+]
